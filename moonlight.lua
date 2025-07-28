17b1f1b9a2418857b3ae91d2fe198f2c46d46fd4
Kurayami-Aru <huyvuong671333@gmail.com>
2025-07-28 16:01:29 +0700

    Update moonlight.lua

--- moonlight.lua
+++ moonlight.lua
@@ -3,11 +3,6 @@ local Library = loadstring(game:HttpGet("https://pastefy.app/Ntt9ayWF/raw"))()
 local main = Library.new()
 
 local rage = main:create_tab('Blatant', 'rbxassetid://76499042599127')
-local player = main:create_tab('Player', 'rbxassetid://126017907477623')
-local world = main:create_tab('World', 'rbxassetid://85168909131990')
-local farm = main:create_tab('Farm', 'rbxassetid://132243429647479')
-local misc = main:create_tab('Misc', 'rbxassetid://132243429647479')
-
 
 
 repeat task.wait() until game:IsLoaded()
