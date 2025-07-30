2b7648ebdda58df5a5d23b9b8cad4d6a15a31ac1
Kurayami-Aru <huyvuong671333@gmail.com>
2025-07-30 20:39:39 +0700

    Update Moonlight.lua

--- Moonlight.lua
+++ Moonlight.lua
@@ -1355,7 +1355,7 @@ customTab.create_description_toggle({
     end
 })
 
-customTab.create_createinput({
+customTab.create_input({
   name = "Sword name"
   placeholder = "Enter Skin name (Case Sensitive)"
   Callback = function(v)
@@ -1365,4 +1365,83 @@ customTab.create_createinput({
       getgenv().updateSword()
     end
 })
-						
+
+local function getSlash(name)
+    local s = swords:GetSword(name)
+    return (s and s.SlashName) or "SlashEffect"
+end
+
+getgenv().config.slash = getSlash(getgenv().config.fx)
+
+local ctrl
+for _, conn in ipairs(getconnections(ReplicatedStorage.Remotes.FireSwordInfo.OnClientEvent)) do
+    if conn.Function and islclosure(conn.Function) then
+        local u = getupvalues(conn.Function)
+        if #u == 1 and typeof(u[1]) == "table" then
+            ctrl = u[1]
+            break
+        end
+    end
+end
+
+local function setSword()
+    if not getgenv().config.enabled then return end
+    local char = LocalPlayer.Character
+    if not char then return end
+
+    setupvalue(rawget(swords, "EquipSwordTo"), 2, false)
+    swords:EquipSwordTo(char, getgenv().config.model)
+    if ctrl then ctrl:SetSword(getgenv().config.anim) end
+end
+
+getgenv().updateSword = function()
+    getgenv().config.slash = getSlash(getgenv().config.fx)
+    setSword()
+end
+
+local playFx
+for _, conn in ipairs(getconnections(ReplicatedStorage.Remotes.ParrySuccessAll.OnClientEvent)) do
+    if conn.Function and debug.getinfo(conn.Function).name == "parrySuccessAll" then
+        playFx = conn.Function
+        conn:Disable()
+        break
+    end
+end
+
+ReplicatedStorage.Remotes.ParrySuccessAll.OnClientEvent:Connect(function(...)
+    local a = {...}
+    if tostring(a[4]) == LocalPlayer.Name and getgenv().config.enabled then
+        a[1], a[3] = getgenv().config.slash, getgenv().config.fx
+    end
+    return playFx(unpack(a))
+end)
+
+Players.LocalPlayer.CharacterAdded:Connect(function()
+    task.delay(1, function()
+        if getgenv().config.enabled then
+            getgenv().updateSword()
+        end
+    end)
+end)
+
+task.spawn(function()
+    while task.wait(1.5) do
+        if getgenv().config.enabled then
+            local char = LocalPlayer.Character
+            if not char then continue end
+
+            if LocalPlayer:GetAttribute("CurrentlyEquippedSword") ~= getgenv().config.model
+            or not char:FindFirstChild(getgenv().config.model) then
+                setSword()
+            end
+
+            for _, obj in char:GetChildren() do
+                if obj:IsA("Model") and obj.Name ~= getgenv().config.model then
+                    obj:Destroy()
+                end
+            end
+        end
+    end
+end)
+
+getgenv().updateSword()
